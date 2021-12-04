9658a7f1853fe245c38cc8fb4d129df48eb93dba
Ch-Adrian <aderter116@gmail.com>
2021-12-04 16:42:24 +0100

    Entry point of project EvolutionGenerator

---
+++ src/main/java/agh/ics/oop/proj1/Main.java
@@ -0,0 +1,10 @@
+package agh.ics.oop.proj1;
+
+import agh.ics.oop.proj1.controller.App;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class,args );
+    }
+}
---
+++ src/main/java/agh/ics/oop/proj1/controller/App.java
@@ -0,0 +1,11 @@
+package agh.ics.oop.proj1.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
